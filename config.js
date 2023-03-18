749f6ed30d79fbcb1381b4f34423f7d1d857189f
AdolescenteRenegado <99355190+AdolescenteRenegado@users.noreply.github.com>
2023-03-18 10:07:42 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,14 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 var firebaseConfig = {
-  //Cole sua Configuração do Firebase aqui
-  
+  apiKey: "AIzaSyAr1qkLxVoTdtFPJnRkI5xTWT29XXAF2-c",
+  authDomain: "ciclista-eletronico-parte-4.firebaseapp.com",
+  projectId: "ciclista-eletronico-parte-4",
+  storageBucket: "ciclista-eletronico-parte-4.appspot.com",
+  messagingSenderId: "343555985544",
+  appId: "1:343555985544:web:980dd63cecf527c4ec8f3d"
 };
+  
 
 firebase.initializeApp(firebaseConfig);
 
